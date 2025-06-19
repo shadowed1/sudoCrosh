fbb8810af151d8f8256a3e2f30e8d7eeccf308ea
shadowed1 <itszachg@gmail.com>
2025-06-19 17:57:07 -0400

    Update sudocrosh_downloader.sh

--- sudocrosh_downloader.sh
+++ sudocrosh_downloader.sh
@@ -7,13 +7,18 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
-echo "${CYAN}${BOLD}Downloading to: /home/chronos/sudocrosh.sh $RESET"
-curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/minioverride.c -o /home/chronos/user/MyFiles/
-curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/sudocrosh.sh -o /home/chronos/sc.sh
-echo ""
 echo "╔═════════════════════════════════════════════════════════════════╗"
 echo "║                                                                 ║"
-echo "║  In Crostini}, copy paste and run:                              ║"
+echo "║  Downloading to: /home/chronos/sudocrosh.sh                     ║"
+echo "║  Downloading to: /home/chronos/user/MyFiles/minioverride.c      ║"
+curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/minioverride.c -o /home/chronos/user/MyFiles/
+curl -L https://raw.githubusercontent.com/shadowed1/ChromeOS_PowerControl/main/sudocrosh.sh -o /home/chronos/sudocrosh.sh
+echo "║                                                                 ║"
+echo "║  Readme:                                                        ║"
+echo "║                                                                 ║"
+echo "╠═════════════════════════════════════════════════════════════════╣"
+echo "║                                                                 ║"
+echo "║  In Crostini, copy paste and run:                               ║"
 echo "║  sudo apt-get update                                            ║"
 echo "║  gcc ~/minioverride.c -o ~/minioverride.so -shared              ║"
 echo "║                                                                 ║"
@@ -25,7 +30,7 @@ echo "║  from My Files/Linux File's to My Files/                        ║"
 echo "║                                                                 ║"
 echo "╠═════════════════════════════════════════════════════════════════╣"
 echo "║                                                                 ║"
-echo "║  In }VT-2 as root run:                                          ║"
+echo "║  In VT-2 as root run:                                           ║"
 echo "║                                                                 ║"
 echo "║  sudo mkdir -p /usr/local/bin                                   ║"
 echo "║                                                                 ║"
@@ -36,5 +41,5 @@ echo "║                                                                 ║"
 echo "║  chmod +x /usr/local/bin/minioverride.so                        ║"
 echo "║                                                                 ║"
 echo "║  bash /usr/local/bin/sc.sh                                      ║"
-    echo "║                                                             ║"
-echo "╠══════════════════════════════════════════════════════════════════════════╣"
+echo "║                                                                 ║"
+echo "╚═════════════════════════════════════════════════════════════════╝"
