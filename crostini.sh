062d5b31888ed772ad8c56924b4111bee1748da7
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-06-12 15:49:12 -0400

    Create crostini.sh

---
+++ crostini.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+if [ -d /mnt/shared/MyFiles/Downloads ]; then
+    SUDOCROSH="/mnt/shared/MyFiles/Downloads/sudocrosh"
+elif [ -d /mnt/chromeos/MyFiles/Downloads ]; then
+    SUDOCROSH="/mnt/chromeos/MyFiles/Downloads/sudocrosh"
+else
+    echo "${RED}Downloads directory not found${RESET}"
+    sleep 5
+    exit 1
+fi
+
+mkdir -p "$SUDOCROSH"
+curl -L https://raw.githubusercontent.com/shadowed1/sudoCrosh/main/minioverride.c -o "$SUDOCROSH/minioverride.c"
+gcc "$SUDOCROSH/minioverride.c" -o "$SUDOCROSH/minioverride.so" -shared
