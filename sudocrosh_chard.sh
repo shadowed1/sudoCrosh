b213d6b549e24d58f371b6fe2cbcf63aad770b87
shadowed1 <22551299+shadowed1@users.noreply.github.com>
2026-09-03 14:30:54 -0400

    Update sudocrosh_chard.sh

--- sudocrosh_chard.sh
+++ sudocrosh_chard.sh
@@ -7,7 +7,6 @@ MAGENTA=$(tput setaf 5)
 CYAN=$(tput setaf 6)
 BOLD=$(tput bold)
 RESET=$(tput sgr0)
-
-mkdir -p /home/chronos/user/MyFiles/Downloads/sudocrosh/
-curl -L https://raw.githubusercontent.com/shadowed1/sudoCrosh/main/minioverride.c -o /home/chronos/user/MyFiles/Downloads/sudocrosh/minioverride.c
-gcc /home/chronos/user/MyFiles/Downloads/sudocrosh/minioverride.c -o /home/chronos/user/MyFiles/Downloads/sudocrosh/minioverride.so -shared
+mkdir -p /home/chronos/user/MyFiles/Downloads/.sudocrosh/
+curl -L https://raw.githubusercontent.com/shadowed1/sudoCrosh/main/minioverride.c -o /home/chronos/user/MyFiles/Downloads/.sudocrosh/minioverride.c
+gcc /home/chronos/user/MyFiles/Downloads/sudocrosh/minioverride.c -o /home/chronos/user/MyFiles/Downloads/.sudocrosh/minioverride.so -shared
